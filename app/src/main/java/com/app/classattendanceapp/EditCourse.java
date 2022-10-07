42a6db4555d65b6b7285663e5a164e5e724a06c2
Aaron Mkandawire <aaronmk2001@gmail.com>
2022-10-07 12:15:31 +0200

    update: add edit course activity

---
+++ app/src/main/java/com/app/classattendanceapp/EditCourse.java
@@ -0,0 +1,54 @@
+package com.app.classattendanceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.app.classattendanceapp.entities.Course;
+import com.app.classattendanceapp.models.CourseModel;
+import com.app.classattendanceapp.state.GlobalState;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class EditCourse extends AppCompatActivity {
+    TextInputEditText courseCode, courseName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_course);
+
+        courseCode = findViewById(R.id.course_code);
+        courseName = findViewById(R.id.course_name);
+
+        courseCode.setText(GlobalState.tmpCourse.getCourseCode());
+        courseName.setText(GlobalState.tmpCourse.getCourseName());
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Edit Course");
+        getSupportActionBar().setSubtitle("Fill the form below");
+    }
+
+    public void editCourse(View v){
+        Toast.makeText(this, GlobalState.tmpCourse.getCourseCode(), Toast.LENGTH_SHORT).show();
+        if(
+            courseCode.getText().toString().equals("") ||
+            courseName.getText().toString().equals("")
+        ){
+            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+        }else{
+
+            GlobalState.tmpCourse.setCourseCode(courseCode.getText().toString());
+            GlobalState.tmpCourse.setCourseName(courseName.getText().toString());
+
+            Context context = getApplicationContext();
+            CourseModel cm = new CourseModel(context);
+            cm.update(GlobalState.tmpCourse);
+            Toast.makeText(this, "Course Successfully Updated", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+}
