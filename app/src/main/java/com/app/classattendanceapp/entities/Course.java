aa954f9537513c99bbfd23b7a07e5f784ea1d3f4
Aaron Mkandawire <aaronmk2001@gmail.com>
2022-10-07 12:13:10 +0200

    update: add course code instance variable

--- app/src/main/java/com/app/classattendanceapp/entities/Course.java
+++ app/src/main/java/com/app/classattendanceapp/entities/Course.java
@@ -2,15 +2,22 @@ package com.app.classattendanceapp.entities;
 
 public class Course {
     private int courseID;
+    private String courseCode;
     private String courseName;
 
-    public Course(int courseID, String courseName) {
+    public Course(
+            int courseID,
+            String courseCode,
+            String courseName
+    ) {
         this.courseID = courseID;
+        this.courseCode = courseCode;
         this.courseName = courseName;
     }
 
     public Course() {
         this.courseID = 0;
+        this.courseCode = "";
         this.courseName = "";
     }
 
@@ -28,5 +35,26 @@ public class Course {
 
     public void setCourseName(String courseName) {
         this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getListViewable(){
+        return this.courseCode + " " + this.courseName;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseID=" + courseID +
+                ", courseCode='" + courseCode + '\'' +
+                ", courseName='" + courseName + '\'' +
+                '}';
     }
 }
