ebd18595ef6e6ddd2e4b5aa1075b8d8d08c368af
Aaron Mkandawire <aaronmk2001@gmail.com>
2022-10-07 12:17:05 +0200

    feat: create global state

---
+++ app/src/main/java/com/app/classattendanceapp/state/GlobalState.java
@@ -0,0 +1,9 @@
+package com.app.classattendanceapp.state;
+
+import com.app.classattendanceapp.entities.Course;
+import com.app.classattendanceapp.entities.Student;
+
+public class GlobalState {
+    public static Student tmpStudent = null;
+    public static Course tmpCourse = null;
+}
